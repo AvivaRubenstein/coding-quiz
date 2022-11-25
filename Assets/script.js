dbc9b5580039096293ae147b9ba917e34cac7f8d
AvivaRubenstein <avivadrubenstein@gmail.com>
2022-11-24 22:38:27 -0500

    solved how to insert multiple lines of HTML into .innerHTML, and used data attributes, so that the intro section would be fully replaced by question 1 rather than just disappear

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,17 @@
 var beginButton = document.querySelector("#begin");
-var intro = document.querySelector("#intro");
-var q1 = document.querySelector("#question1");
-
-q1.style.display="none";
+var container = document.querySelector(".container");
+var correct = document.querySelector(".correct");
 
 beginButton.addEventListener("click", function (event){
-    intro.innerHTML=" ";
-    q1.style.display="block";
+    container.innerHTML ="<h1>Which line of code is used to prevent event bubbling in javaScript?</h1>" +
+    "<button class = 'choice' class = 'correct'>event.stopPropogation()</button>" +
+    "<button class = 'choice'>event.stopBubbling()</button>" +
+    "<button class = 'choice'>event.stopClickTrigger()</button>"+
+    "<button class = 'choice'>event.preventDefault()</button>";
+    container.dataset.stage="question1";
+})
+
+correct.addEventListener("click", function(event) {
+    correct.setAttribute("style", "background-color: #00ff00;");
+
 })
