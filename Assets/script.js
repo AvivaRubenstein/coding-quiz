9cff5443825fb6fd9a891a85e5bf1b9b680ba743
AvivaRubenstein <avivadrubenstein@gmail.com>
2022-11-28 16:24:23 -0500

    moved the intro content from the HTML file to the JS file, and used JS to create elements for the intro information and the first question elements (intro elements will clear out and be replaced by question elements when click event occurs)

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,49 @@
-var beginButton = document.querySelector("#begin");
 var container = document.querySelector(".container");
 var correct = document.querySelector(".correct");
+var question = document.createElement("h1");
+var choice1 = document.createElement("button");
+var choice2 = document.createElement("button");
+var choice3 = document.createElement("button");
+var choice4 = document.createElement("button");
 
-beginButton.addEventListener("click", function (event){
-    container.innerHTML ="<h1>Which line of code is used to prevent event bubbling in javaScript?</h1>" +
-    "<button class = 'choice' class = 'correct'>event.stopPropogation()</button>" +
-    "<button class = 'choice'>event.stopBubbling()</button>" +
-    "<button class = 'choice'>event.stopClickTrigger()</button>"+
-    "<button class = 'choice'>event.preventDefault()</button>";
-    container.dataset.stage="question1";
+var h1  = document.createElement("h1");
+h1.innerHTML=" <h1>Coding Quiz Challenge</h1>";
+container.appendChild(h1);
+var p = document.createElement("p");
+p.innerHTML = "<p>Try to answer the following code-related questions to practice for coding interviews! You will have 10 seconds to answer each question.  Click 'Begin' to start!</p>";
+container.appendChild(p);
+var begin = document.createElement("button");
+begin.innerHTML ="<button id = 'begin'>Begin</button>";
+container.appendChild(begin);
+
+//when called, this function will clear the HTML content from the intro
+function clearHTML(){
+    h1.innerHTML="";
+    p.innerHTML = "";
+    begin.innerHTML ="";
+}
+//when the begin button is pressed, the intro content will disappear and the first question will be displayed.
+begin.addEventListener("click", function (event){
+    clearHTML();
+    //for each question, we are creating an h1 element and 4 button elements
+    //they are declared as variables at the top of the page so we aren't limited by scope
+    //the HTML content for each is put in with .innerHTML, and we must append them to the container for them to actually become part of the HTML file
+    question.innerHTML ="<h1>Which line of code is used to prevent event bubbling in javaScript?</h1 ";
+    container.appendChild(question);
+    
+    //the correct answer is identified here with the class "correct" so that we can treat all correct answers the same when when clicked
+    choice1.innerHTML="<button class = 'choice' class = 'correct'> event.stopPropogation()</button>";
+    container.appendChild(choice1);
+   
+    choice2.innerHTML="<button class = 'choice'>event.stopBubbling()</button>";
+    container.appendChild(choice2);
+   
+    choice3.innerHTML="<button class = 'choice'>event.stopClickTrigger()</button>";
+    container.appendChild(choice3);
+   
+    choice4.innerHTML="<button class = 'choice'>event.preventDefault()</button>";
+    container.appendChild(choice4);
+   
 })
 
 correct.addEventListener("click", function(event) {
