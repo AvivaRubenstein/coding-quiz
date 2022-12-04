3773aef3b3822b3c2939b7902463ccf5030480f7
AvivaRubenstein <avivadrubenstein@gmail.com>
2022-12-04 14:02:05 -0500

    miscellaneous clean up of code, and added more comments to explain code

--- Assets/script.js
+++ Assets/script.js
@@ -12,11 +12,17 @@ var btnClicked;
 //start with the numRight and Wrong equal to 0, need to increment the relvant var by 1 after each answer
 var numRight = 0;
 var userName;
-var submitName;
 var listNames = [];
 var listScores = [];
 var form;
 var submitBtn;
+var scoreStorage;
+var scoresArray;
+var correctOrNotMessage;
+//the timer will start with 50 seconds left
+var timerEl = document.querySelector("#timer");
+var secondsLeft = 50;
+
 
 
 var h1 = document.createElement("h1");
@@ -42,9 +48,9 @@ function clearHTML() {
 
 //when the begin button is pressed, the intro content will disappear and the first question will be displayed.
 begin.addEventListener("click", function (event) {
-    //TODO: look into event bubbling!
     event.stopPropagation();
     clearHTML();
+    //creates the timer
     setTime();
     //for each question, we are creating an h1 element and 4 button elements
     //they are declared as variables at the top of the page so we aren't limited by scope
@@ -72,6 +78,10 @@ begin.addEventListener("click", function (event) {
     choice4.setAttribute("class", "choice");
     qCard.appendChild(choice4);
 
+    //After the user answers a question, this HTML element will display a message indicating whether their last answer was correct or not
+    correctOrNotMessage = document.createElement("h4");
+    qCard.appendChild(correctOrNotMessage);
+
     choice = document.querySelectorAll(".choice");
 
     //checks each choice to see if it is marked as correct 
@@ -84,19 +94,21 @@ begin.addEventListener("click", function (event) {
     for (x = 0; x < choice.length; x++) {
         // console.log(choice.length)
         choice[x].addEventListener("click", function (event) {
-
+            
             btnClicked = event.target;
             if (btnClicked === correct) {
                 numRight++;
+                correctOrNotMessage.textContent = "Correct!"
             } else {
                 secondsLeft -= 5;
-
+                correctOrNotMessage.textContent = "Incorrect."
             }
 
             //sets us up to move to the next question in our questionBlock
             currentQ++;
+            //if we have reached the end of the number of qs we have, this removes the question/answers and stops the timer
             if (currentQ >= promptBank.length) {
-                //TODO erase question block
+                //erases question block
                 question.remove();
                 choice1.remove();
                 choice2.remove();
@@ -107,7 +119,9 @@ begin.addEventListener("click", function (event) {
 
 
             } else {
+                //if we still have more questions left, we call the setQuestion() function to update the questionBlock object to be the next q in the arrays
                 setQuestion();
+                //the slots for the question and the 4 choices will be filled with the current content of the object's fields
                 question.innerHTML = `${questionBlock.prompt}`;
                 choice1.innerHTML = `${questionBlock.ans1}`;
                 choice1.setAttribute("class", "choice");
@@ -117,30 +131,33 @@ begin.addEventListener("click", function (event) {
                 choice3.setAttribute("class", "choice");
                 choice4.innerHTML = `${questionBlock.ans4}`;
                 choice4.setAttribute("class", "choice");
+                //this will assign the id "correct" to whichever of the buttons contains the correct answer 
                 checkCorrectness();
             }
         });
     }
 
     function setTime() {
-        // Sets interval in variable
+        // Sets interval/timer --- it displays a countdown that decrements every second
         var timerInterval = setInterval(function () {
             secondsLeft--;
             timerEl.textContent = secondsLeft + " seconds left";
 
-
-
-            if (secondsLeft <= 0) {
-                //TODO: clear out question block when timer runs out, not just when we run out of qs
-                // Stops execution of action at set interval
+            //If we run through all the questions, we don't need the timer to continue, and we want to stop TimeoutMessage() from running a second time when the timer also runs out
+            //so the timer should stop when we reach the end of the questions
+            if(currentQ >= promptBank.length) {
                 clearInterval(timerInterval);
-                //TODO erase question block
+            }
+            if (secondsLeft <= 0) {
+                // Stops the timer when we run out of time
+                clearInterval(timerInterval);
+                //erases the question block when time runs out
                 question.remove();
                 choice1.remove();
                 choice2.remove();
                 choice3.remove();
                 choice4.remove();
-                // Calls function to create and append image
+                // Calls function to get rid of timer, display the score, and allow user to input their name to be stored with the past scores
                 timeoutMessage();
             }
 
@@ -167,30 +184,31 @@ function setQuestion() {
     };
 }
 
-
+//these arrays represent the question and answer content for our quiz, with each index representing the next q in the sequence)
 var promptBank = ["Which line of code is used to prevent event bubbling in javaScript?",
     "What is a for loop used to do?",
-    "What does DOM refer to in HTML?"];
+    "What does DOM refer to in HTML?", "To create new HTML elements using javaScript, you must: ", "Local storage allows us to: "];
 var ans1Bank = ["event.stopPropogation()",
     "It sets up conditional logic to run indefinitely",
-    "The Direct Outer Method "];
+    "The Direct Outer Method ", "You can't add it with javaScript, you must put it directly into the HTML code", "Store information in the user's browser even after they refresh or close the page"];
 var ans2Bank = ["event.stopBubbling()",
     "It tells you what each block of code is used for",
-    "The Document Object Model"];
+    "The Document Object Model", "use the addNewHTML item class", "Store information in the user's browser, but only until they refresh or close the page, at which point it deletes"];
 var ans3Bank = ["event.stopClickTrigger()",
     "It iterates through a block of code multiple times",
-    "The Document Over Main"];
+    "The Document Over Main", "create a new CSS class and link it to javaScript", "Store information in an external server until the user finishes their browser session"];
 var ans4Bank = ["event.preventDefault()",
     "It defines a boolean variable ",
-    "The Direct Object Method"];
+    "The Direct Object Method", "create a new HTML element and append it to the document", "Store information to the user's browser and to our server until the user chooses to clear their cookies"];
 
+//these arrays store which of the 4 answer choices is correct (each index represents another q in the sequence)
+var isCorrect1 = [true, false, false, false, true];
+var isCorrect2 = [false, false, true, false, false];
+var isCorrect3 = [false, true, false, false, false];
+var isCorrect4 = [false, false, false, true, false];
 
-var isCorrect1 = [true, false, false];
-var isCorrect2 = [false, false, true];
-var isCorrect3 = [false, true, false];
-var isCorrect4 = [false, false, false];
-
-
+//this function assigns the id of correct to the choice which has the correct answer
+//incorrect answers have this id removed so that the buttons which contained past correct answers are not still marked as correct
 function checkCorrectness() {
     if (isCorrect1[currentQ] === true) {
         choice1.setAttribute("id", "correct");
@@ -215,18 +233,16 @@ function checkCorrectness() {
     correct = document.querySelector("#correct");
 }
 
-var timerEl = document.querySelector("#timer");
-var secondsLeft = 70;
 
-
+var quizOver;
 function timeoutMessage() {
     //this function should get rid of the timer element, and create a message saying the quiz is over, calculate and report the current quiz score
     //and create a form for the user to submit their name to be stored in the high score list
     $("#timer").remove();
-    var quizOver = document.createElement("h2");
+    quizOver = document.createElement("h2");
     quizOver.innerHTML = `All done!  Your final score is ${numRight}.`;
     container.appendChild(quizOver);
-    var enterName = document.createElement("form");
+    enterName = document.createElement("form");
     enterName.innerHTML = "<label for='name'> Enter your name to save your high score:</label> <br> <input type='text' id='userName' name='name'><br> <input type='submit' value='Submit' id='submit'>";
     enterName.setAttribute = ("id", "form");
     container.appendChild(enterName);
@@ -236,16 +252,19 @@ function timeoutMessage() {
 
 
 
-
+//this event listener listens for the submit button being clicked, sets up the scoreStorage object to contain the current user's score and entered name
     submitBtn.addEventListener("click", function (event) {
         event.preventDefault();
-        var scoreStorage = {
+        scoreStorage = {
             currentUser: userName.value,
             currentScore: numRight
         };
-        var scoresArray = [];
+        scoresArray = [];
         var lastPlayer = JSON.parse(localStorage.getItem("score-name"));
 
+        //if there is no previous name/score stored in score-name, we do localStorage.setItem set up the score-name in local storage
+        //otherwise, we retrieve the existing entries with localStorage.getItem, and push the newest scores (in the scoreStorage object) into our scoresArray, and then set
+        //the new/update scoresArray array to be stored in local storage under the score-name key
         if (lastPlayer === null) {
             scoresArray.push(scoreStorage);
             localStorage.setItem("score-name", JSON.stringify(scoresArray));
@@ -255,9 +274,29 @@ function timeoutMessage() {
             scoresArray.push(scoreStorage);
             localStorage.setItem("score-name", JSON.stringify(scoresArray));
         }
-
+        //after the user submits their name, 
+        //calls function to display the previous high scores/names
+        handleHighScoreDisplay();
 
     });
 
 }
 
+function handleHighScoreDisplay (){
+    //clear out the message which told the user whether their last answer was correct or not
+    //clear out quizOver message and the enterName form
+    correctOrNotMessage.remove();
+    quizOver.remove();
+    enterName.remove();
+
+    var highScoreList = document.createElement("ul");
+    container.appendChild(highScoreList);
+    for (x=0; x < scoresArray.length; x++){
+
+    liEl = document.createElement("li");
+    highScoreList.appendChild(liEl);
+    liEl.innerHTML = `<span class = highScoreName> Name:   </span> ${scoresArray[x].currentUser } <span class = highScoreScore>   Score:   </span> ${scoresArray[x].currentScore}`;
+    }
+    
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # coding-quiz
+
+## Description
+This project is a coding quiz designed to test the user's knowledge of selected coding topics.  It asks the user a series of questions, marks their answers as correct or incorrect, and stores their names with their scores in local storage so that quiz scores can be accessed after the quiz is complete.  The quiz has a timer which allots the user 10 seconds per question, and causes the quiz to timeout when the allotted time is used up.
+
+The project allowed me to practice working with time intervals, event listeners, local storage, and web APIs.  It also allowed me to practiced working with targeting, appending, and removing elements from the HTML DOM.
+
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
